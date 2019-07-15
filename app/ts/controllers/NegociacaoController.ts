2a6fee708a548583beef624a3af839fccf7de497
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-15 19:28:31 -0300

    Adc ENUM

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -25,20 +25,44 @@ export class NegociacaoController{
     adiciona(event: Event){
         event.preventDefault();//Não atualizar a página depois de submeter o formulário
         
-        const negociacao = new Negociacao(
-            //Date aceita uma string com '2019,04,07' porém está vindo com '2019-04-07'
+        //Date aceita uma string com '2019,04,07' porém está vindo com '2019-04-07'
             //Por isso é feito o replace, de - para ,
-            new Date(this._inputData.val().replace(/-/g, ',')), 
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        //Se for VERDADE é pq NÃO é dia da semana, por isso usamos o '!'
+        if(!this._ehDiaUtil(data)) {
+            this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+            return;
+        }
+
+        const negociacao = new Negociacao(
+            data, 
             parseInt(this._inputQuantidade.val()), //Converto para INT
             parseFloat(this._inputValor.val()) //Converto para FLOAT
         );
 
         this._negociacoes.adiciona(negociacao);
-        
         this._negociacoesView.update(this._negociacoes);
-
         this._mensagemView.update('Negociação cadastrada com sucesso!');
     }
 
 
+    //Se a data NÃO FOR IGUAL a Sábado nem Domingo ele retorna TRUE
+    private _ehDiaUtil(data: Date) {
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+
+
+}
+
+//Em ordem começando do 0.... (mantive a ordem pois em JS essa é a ordem para validar os dias da semana)
+//Posso atribuir os valores caso eu queira
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta, 
+    Quinta, 
+    Sexta, 
+    Sabado, 
 }
