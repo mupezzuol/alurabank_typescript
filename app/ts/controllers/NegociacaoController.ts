465adb2f8701fdc7fb5b0b49a2a9457e57d9f4a4
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-15 15:01:31 -0300

    HTMLInputElement; CAST com TS;

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,18 +1,20 @@
 class NegociacaoController{
 
-    private _inputData;
-    private _inputQuantidade;
-    private _inputValor;
+    //Quando vem do HTML/FORM é um Element o tipo que retorna, porém temos que especificar para acessar métodos mais especificos
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
 
-
+    
+    //Fazendo CAST do tipo <HTMLInputElement> ->> Estou dando ctz que será retornado valores de input, não de TAG's
     constructor(){
-        this._inputData = document.querySelector('#data');//id do input no formulário
-        this._inputQuantidade = document.querySelector('#quantidade');//id do input no formulário
-        this._inputValor = document.querySelector('#valor');//id do input no formulário
+        this._inputData = <HTMLInputElement>document.querySelector('#data');//id do input no formulário
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');//id do input no formulário
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');//id do input no formulário
     }
 
 
-    adiciona(event){
+    adiciona(event: Event){
         event.preventDefault();//Não atualizar a página depois de submeter o formulário
 
         const negociacao = new Negociacao(
