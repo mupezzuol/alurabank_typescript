42592ed7de7f75c69c3ef8c5db7f6e36d81e2dcb
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-15 17:39:26 -0300

    Adc JQuery; Calando o Compilador para aceitar JQuery;

--- app/js/views/View.js
+++ app/js/views/View.js
@@ -1,11 +1,8 @@
-//T é tipo Genérico, na hora de herdar podemos passar qualquer tipo... Array, String, Object etc...
-//Todos os lugares que tiverem T seráo trocados pelo modelo colocado na hora de herdar
-//Criamos uma classe abstract, ou seja, não podemos criar uma instancia dela ->>> view = new View('teste');
 class View {
     constructor(seletor) {
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
     update(model) {
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 }
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,17 +1,19 @@
 //T é tipo Genérico, na hora de herdar podemos passar qualquer tipo... Array, String, Object etc...
 //Todos os lugares que tiverem T seráo trocados pelo modelo colocado na hora de herdar
 //Criamos uma classe abstract, ou seja, não podemos criar uma instancia dela ->>> view = new View('teste');
+declare var $:any; //Declarando VAR do JQuery -> Mando calar a boca do compilador e ele entender essa declaração
+
 abstract class View<T> {
 
     //protected -> Somente membros da propria classe e seus filhos podem acessar
-    private _elemento: Element;
+    private _elemento: any;//Precisa ser qualquer coisa para aceitar o jQuery
 
     constructor(seletor: string) {
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
 
     update(model: T) {
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 
     //Método Abstrato -> Quem herdar essa classe DEVERÁ OBRIGATORIAMENTE implementar esse método
